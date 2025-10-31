5afc5f35fd9957c2c585aa2f12faef0a5a81bf5f
Ronen Begelfor <rbegelfor@rrc.ca>
2025-10-31 17:58:43 -0500

    Implemented PY_LINT fixes.

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -1,3 +1,5 @@
+"""Example script from Assignment 1 to use bandit on."""
+
 import os
 import pymysql
 from urllib.request import urlopen
@@ -6,21 +8,25 @@ db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
 
 
 def get_user_input():
+    """Ask the user for their name and return it."""
     user_input = input("Enter your name: ")
     return user_input
 
 
 def send_email(to, subject, body):
+    """Send an email using a system command."""
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
 
 def get_data():
+    """Fetch data from API and return it as a string."""
     url = "http://insecure-api.com/get-data"
     data = urlopen(url).read().decode()
     return data
 
 
 def save_to_db(data):
+    """Save provided data to the database."""
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
@@ -31,6 +37,7 @@ def save_to_db(data):
 
 
 if __name__ == "__main__":
+    """Main entry point of the script."""
     user_input = get_user_input()
     data = get_data()
     save_to_db(data)
